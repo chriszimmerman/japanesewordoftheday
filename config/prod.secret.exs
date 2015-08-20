9f4746d7654fc934df88a97d646f4761e9d39ebe
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-08-19 20:58:41 -0400

    Added environment variables for database connection info

---
+++ config/prod.secret.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :japanese, Japanese.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :japanese, Japanese.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("USER"),
+  password: System.get_env("PASSWORD"),
+  database: System.get_env("DB"),
+  size: 20 # The amount of database connections in the pool
