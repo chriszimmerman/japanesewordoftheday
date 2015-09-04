556ab7b815957fa0fbdd8fddd181f05af75f4f4b
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-09-04 11:17:54 -0400

    Added csv file to read seed data from. Added script to put seed data into database in seeds.exs.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+
+{:ok, result} = File.read "japanese_jlpt1_lite.csv"
+lines = String.split(result, "\n")
+purged_lines = List.delete(lines, "")
+Enum.each(purged_lines, 
+			fn row -> 
+				[kanji, furigana, romaji, english] = String.split(row, ","); 
+				Japanese.Repo.insert!(%Japanese.Word{ kanji: kanji, furigana: furigana, romaji: romaji, english: english })
+			end)
+
