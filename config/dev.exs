fcaf7c773a78c340c180f49141f2137404ba524e
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-09-09 21:29:52 -0400

    Removed usage of deprecated pool atom.

--- config/dev.exs
+++ config/dev.exs
@@ -32,4 +32,4 @@ config :japanese, Japanese.Repo,
   username: "postgres",
   password: "postgres",
   database: "japanese_dev",
-  size: 10 # The amount of database connections in the pool
+  pool_size: 10 # The amount of database connections in the pool
